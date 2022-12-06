85e8a03746370499f02b7d652f27d9bd7a988c79
Mikey Elmers <elmers@lst.uni-saarland.de>
2022-12-06 14:29:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+[![orcid](https://img.shields.io/badge/ORCID-0000--0002--3929--788X-green?style=plastic&logo=orcid&url=https://orcid.org/0000-0002-3929-788X)](https://orcid.org/0000-0002-3929-788X)
+
+# Pause-internal particles in university lectures
+This repository contains the data and code for "" by Mikey Elmers and Jürgen Trouvain for the 20th International Congress of Phonetic Sciences (ICPhS '23), Prague, August 7-11.
---
+++ scripts/00_renvRestore.R
@@ -0,0 +1,17 @@
+############
+### Info ###
+############
+#
+# Project: University Lecture Listening Task
+#
+# Conference: 20th International Congress of Phonetic Sciences (ICPhS '23)
+#
+# Contributors: Mikey Elmers and Jürgen Trouvain
+#
+# Part 00: Restore project dependencies from lockfile (renv.lock)
+#
+# Date: December 06, 2022
+#
+#-------------------------------------------------------
+# restore packages
+renv::restore()
